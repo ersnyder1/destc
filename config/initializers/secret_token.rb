04317203fd224bc7dbd175d6d9d2979cb469b1c7
Eric Snyder <ersnyder1@gmail.com>
2014-04-09 22:21:30 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Destc::Application.config.secret_key_base = '01ddc3a08eb02a479003a246dbdd328c85cbf86f72eeb2ff56f88e12adcb62cc453eef95f325005025cffa851501f8486d5e54df00c2f356133c7c27ebca2614'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Destc::Application.config.session_store :cookie_store, key: '_destc_session'
